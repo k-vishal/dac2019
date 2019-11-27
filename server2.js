b70705417b0809bef06d02c21330f04300246a05
vishal <k.vishalkumbhar@gmail.com>
2019-11-27 15:28:22 +0530

    fifth commit for server2

--- server2.js
+++ server2.js
@@ -32,5 +32,12 @@ app.listen(port, ()=>{
     console.log("Server Started..");
 });
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 
 
