40fcd75e914aa1d424d9fa6c9deba9c805f35569
vishal <k.vishalkumbhar@gmail.com>
2019-11-27 15:26:41 +0530

    fourth commit for server2

--- server2.js
+++ server2.js
@@ -14,12 +14,6 @@ app.use(function(req, res, next) {
 });
 
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
 
 
 app.use(function(req, res, next) {
